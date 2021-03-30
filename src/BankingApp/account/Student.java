511417467cbd64edfe6dd89358452117cc039e27
IsaacSxm <rhymefirst1@gmail.com>
2021-03-30 14:22:05 +0200

    Package accounts *

--- src/BankingApp/account/Student.java
+++ src/BankingApp/account/Student.java
@@ -1,4 +1,77 @@
-package BankingApp.account;
+package com.BankingApp.accounts;
 
-public class Student {
+import com.BankingApp.Exceptions.InsufficientFundsException;
+import com.BankingApp.Person.User;
+import com.BankingApp.message.Message;
+import com.BankingApp.payments.*;
+
+import java.util.ArrayList;
+
+public class Student implements Account {
+
+    protected double money;
+    protected ArrayList<String> transactionHistory;
+    protected int accountNumber;
+    protected User user;
+    protected ArrayList<Message> messages;
+    private final static double interestRate = 0.5;
+
+    public Student(User user, int accountNumber) {
+        this.user = user;
+        this.accountNumber = accountNumber;
+    }
+
+    @Override
+    public String getAccountDetails() {
+        return "Account number: " + this.accountNumber + "My account name: " + this.user.getName();
+    }
+
+    @Override
+    public double getAccountBalance() {
+        return this.money;
+    }
+
+    @Override
+    public ArrayList<Message> getMessages() {
+        return this.messages;
+    }
+
+    @Override
+    public void setMessages(Message message) {
+        this.messages.add(message);
+    }
+
+    @Override
+    public void setAccountBalance(double money) {
+        this.money = money;
+    }
+
+    @Override
+    public ArrayList<String> getTransactionHistory() {
+        return this.transactionHistory;
+    }
+
+    @Override
+    public int getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    @Override
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    @Override
+    public void makePayment(CardPayment cardPayments, double cost) throws InsufficientFundsException {
+        this.money -= cost;
+        if (money < cost) {
+            throw new InsufficientFundsException();
+        }
+        cardPayments.makePayment(cost);
+    }
+
+    public void depositMoney(double amountToDeposit) {
+        double money = amountToDeposit + ((amountToDeposit * interestRate) / 100);
+        this.money += money;
+    }
 }
