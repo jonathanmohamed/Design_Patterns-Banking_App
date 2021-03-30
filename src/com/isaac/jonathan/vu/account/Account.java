a0993d93f3db7778d96fc2180852640f81f7ad73
IsaacSxm <rhymefirst1@gmail.com>
2021-03-30 14:06:02 +0200

    Coded account.

--- src/com/isaac/jonathan/vu/account/Account.java
+++ src/com/isaac/jonathan/vu/account/Account.java
@@ -1,5 +1,31 @@
 package com.isaac.jonathan.vu.account;
 
+import com.BankingApp.Exceptions.InsufficientFundsException;
+import com.BankingApp.message.Message;
+import com.BankingApp.payments.CardPayment;
+
+import java.util.ArrayList;
+
 public interface Account {
+
+    String getAccountDetails();
+
+    double getAccountBalance();
+
+    ArrayList<Message> getMessages();
+
+    void setMessages(Message message);
+
+    void setAccountBalance(double money);
+
+    ArrayList<String> getTransactionHistory();
+
+    int getAccountNumber();
+
+    void setAccountNumber(int accountNumber);
+
+    void makePayment(CardPayment cardPayments, double cost) throws InsufficientFundsException;
+
+    void depositMoney(double amountToDeposit);
 
 }
